f98563dfc7e9d1d308be4c0fbfe65143fdbc8c29
João Nascimento <joaocleber@outlook.com>
2024-11-09 19:23:50 -0300

    Initial Commit

---
+++ PodcastsRSS/PodcastsRSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastsRSSApp.swift
+//  PodcastsRSS
+//
+//  Created by Joao on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PodcastsRSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
